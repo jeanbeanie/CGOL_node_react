90c7717c35957d4bd62f809dcc4d339d4716afc9
jean <msjeanneramos@gmail.com>
2018-05-15 10:50:21 -0400

    fixed some lint errors and formatting stuff

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,5 +1,4 @@
 /* src/components/HomeContainer.jsx */
-/* @flow */
 
 import React from 'react';
 import Buttons from './Buttons';
@@ -36,7 +35,7 @@ class HomeContainer extends React.Component <{}> {
   // set up interval to run play method
   playButton = () => {
     // start over setInterval for specified intervalId
-    if(this.intervalId){
+    if (this.intervalId) {
       clearInterval(this.intervalId);
     }
     // every interval of this.speed, call this.play
@@ -48,25 +47,25 @@ class HomeContainer extends React.Component <{}> {
 
   // run one generation
   play = () => {
-    let g = this.state.grid;
-    let g2 = this.state.grid;
+    const g = this.state.grid;
+    const g2 = this.state.grid;
 
     // go through every el in grid
     // apply conway's  game of life rules
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
         // init how many neighbors a cell has
         let count = 0;
         // check each neighbor of a certain cell
         // add to count if it exists as true
-        if (i > 0) if (g[i - 1][j]) count++;
-        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
-        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
-        if (j < this.cols - 1) if (g[i][j + 1]) count++;
-        if (j > 0) if (g[i][j - 1]) count++;
-        if (i < this.rows - 1) if (g[i + 1][j]) count++;
-        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
-        if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+        if (i > 0) if (g[i - 1][j]) count += 1;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count += 1;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count += 1;
+        if (j < this.cols - 1) if (g[i][j + 1]) count += 1;
+        if (j > 0) if (g[i][j - 1]) count += 1;
+        if (i < this.rows - 1) if (g[i + 1][j]) count += 1;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count += 1;
+        if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count += 1;
         // determine if cell should die
         // does cell have <2 or more than 3 neighbors? DIE!
         if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
@@ -77,7 +76,7 @@ class HomeContainer extends React.Component <{}> {
     }
     this.setState({
       gridFull: g2,
-      generation: this.state.generation + 1
+      generation: this.state.generation + 1,
     });
   }
 
@@ -92,8 +91,8 @@ class HomeContainer extends React.Component <{}> {
   clearButton = () => {
     const grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
     this.setState({
-        grid,
-        generation: 0,
+      grid,
+      generation: 0,
     });
     this.pauseButton();
   }
@@ -101,27 +100,27 @@ class HomeContainer extends React.Component <{}> {
   // called when user selects an individual box component
   selectBox = (row, col) => {
     // create copy of grid array so we can modify it
-    let grid = this.state.grid;
+    const grid = this.state.grid;
     // flip the bool value of the spot in arr that corresponds to box component
     grid[row][col] = !grid[row][col];
     // set grid to our updated grid
     this.setState({
-      grid: grid,
-    })
+      grid,
+    });
   }
 
   changeSpeed = (speed) => {
     switch (speed) {
     case 'slow':
       this.speed = 1000;
-      if (this.state.generation > 0){
+      if (this.state.generation > 0) {
         this.playButton();
       }
       break;
     default:
     case 'fast':
       this.speed = 100;
-      if (this.state.generation > 0){
+      if (this.state.generation > 0) {
         this.playButton();
       }
     }
@@ -142,14 +141,14 @@ class HomeContainer extends React.Component <{}> {
       this.cols = 70;
       this.rows = 50;
     }
-    this.clearButton()
+    this.clearButton();
   }
   // populates the grid with a random amount of active boxes
   seedButton = () => {
-    let { grid } = this.state;
+    const { grid } = this.state;
     // loop over each value in grid array and inner arrays
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
         // 10% chance to set a box's value to 'true'
         if (Math.floor(Math.random() * 10) === 1) {
           grid[i][j] = true;
@@ -158,8 +157,8 @@ class HomeContainer extends React.Component <{}> {
     }
     // set current grid state to new seeded grid
     this.setState({
-      grid: grid,
-    })
+      grid,
+    });
   }
 
   render() {
